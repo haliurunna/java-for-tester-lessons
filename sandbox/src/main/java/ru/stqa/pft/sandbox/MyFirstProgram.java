f238b0071262be21c235ee634a83d5e0c07318f3
Marina Kupriyanova <mandevilla40@yandex.ru>
2019-04-21 21:31:15 +0300

    functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,19 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		String somebody = "world";
-		System.out.println("Hello, "+ somebody +"!");
+		hello("world");
+		hello("user");
+		hello("Marina");
 
+		double len = 5;
+		System.out.println("S квадрата со стороной " + len + "=" + area(len));
+	}
 
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l) {
+		return l*l;
 	}
 }
